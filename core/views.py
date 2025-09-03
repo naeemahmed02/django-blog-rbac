27d230eb86abcca1f6c589b5f3df30efa6816b8a
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2025-09-03 22:44:45 +0500

    template changed

--- core/views.py
+++ core/views.py
@@ -4,34 +4,6 @@ from category.models import Category
 
 
 def home(request):
-    hero_posts = Post.objects.filter(status="published")[:3]
-    categories = Category.objects.all()
-    # Showing at least one post from each category
-    category_posts = []
-    for category in categories:
-        post = category.posts.filter(status="published").order_by("-created_at").first()
-        if post:
-            category_posts.append(post)
-    # Split posts into 3 columns
-    col_1 = category_posts[0::3]  # every 3rd starting at 0
-    col_2 = category_posts[1::3]  # every 3rd starting at 1
-    col_3 = category_posts[2::3]  # every 3rd starting at 2
-    
-    # Trending posts based on views
-    trending_posts_sidebar = Post.objects.filter(status = 'published').order_by('-views')[:6]
-    
-    # categories
-    categories = Category.objects.all().order_by('-created_at')[:3]
-    
-    
-    context = {
-        "hero_posts": hero_posts,
-        "category_posts": category_posts,
-        "col_1": col_1,
-        "col_2": col_2,
-        "col_3": col_3,
-        'trending_posts_sidebar' : trending_posts_sidebar,
-        'categories' : categories
-    }
-    print(context)
+    posts = Post.objects.filter(status = 'published')[:5]
+    context = {'posts': posts}
     return render(request, "core/index.html", context)
